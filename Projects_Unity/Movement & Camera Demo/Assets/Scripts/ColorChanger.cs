12c596833c6ddc5e7e1962112d46755e7818a417
Highway-Star <lrh8760@nyu.edu>
2022-02-04 08:54:41 -0500

    Movement Demo HW

--- Projects_Unity/Movement & Camera Demo/Assets/Scripts/ColorChanger.cs
+++ Projects_Unity/Movement & Camera Demo/Assets/Scripts/ColorChanger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColorChange : MonoBehaviour
+public class ColorChanger : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
